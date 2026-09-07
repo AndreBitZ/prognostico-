570d040ecd4377ce45ab8a770d6a53e0f9b6d734
AndreBitZ <35458085+AndreBitZ@users.noreply.github.com>
2026-09-07 14:41:16 +0100

    Não quebrar a página se o optimizer de imagens falhar

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "crests.football-data.org" },
       { protocol: "https", hostname: "upload.wikimedia.org" },
